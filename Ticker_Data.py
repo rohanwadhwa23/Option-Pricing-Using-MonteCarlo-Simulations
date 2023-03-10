baa5d2f0cdeac70231a706a10694df954ac164d0
Rohan Wadhwa <rohanwadhwa23@gmail.com>
2023-03-10 02:44:43 -0500

    Update Ticker_Data.py

--- Ticker_Data.py
+++ Ticker_Data.py
@@ -5,8 +5,8 @@ import yfinance as yf
 class Ticker_Data:
 
     def __init__(self):
-        self.ticker_list = pd.read_csv("Data/Tickers.csv")
-        #self.ticker_list = pd.read_csv("Data/Tickers_Full.csv")
+        #self.ticker_list = pd.read_csv("Data/Tickers.csv")
+        self.ticker_list = pd.read_csv("Data/Tickers_Full.csv")
         self.ticker_data = yf.download(self.ticker_list.Ticker.to_list(),'2020-1-1','2022-12-31', auto_adjust=True)#['Close']
             
     def get_ticker_list(self):
